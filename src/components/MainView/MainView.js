e570a4145a2b0f8cdaba0fc08ac96f66ab1c54ff
Eetu Kallio <eetu.kallio@haltu.fi>
2018-12-03 08:18:51 +0200

    Add header to main view

--- src/components/MainView/MainView.js
+++ src/components/MainView/MainView.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
 export default class MainView extends Component {
+  static navigationOptions = {
+    title: 'Home',
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+
   render() {
     return (
       <View>
         <Text> Main View </Text>
       </View>
-    )
+    );
   }
 }
