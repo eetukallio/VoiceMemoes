6b80f7b4c194663c5bd3828a62faf0b5c48ad2ee
Eetu Kallio <eetu.kallio@gmail.com>
2019-01-09 00:19:01 +0200

    Add iOS audio config

--- src/util/recorderConfig.js
+++ src/util/recorderConfig.js
@@ -1,11 +1,22 @@
 import { AudioRecorder } from 'react-native-audio';
+import { Platform } from 'react-native';
 
 export const prepareRecordingPath = audioPath => {
-  AudioRecorder.prepareRecordingAtPath(audioPath, {
-    SampleRate: 8000,
-    Channels: 1,
-    AudioQuality: 'High',
-    AudioEncoding: 'amr_nb',
-    AudioEncodingBitRate: 128000
-  });
+  if (Platform.OS === 'android') {
+    AudioRecorder.prepareRecordingAtPath(audioPath, {
+      SampleRate: 8000,
+      Channels: 1,
+      AudioQuality: 'High',
+      AudioEncoding: 'amr_nb',
+      AudioEncodingBitRate: 128000
+    });
+  } else {
+    AudioRecorder.prepareRecordingAtPath(audioPath, {
+      SampleRate: 16000,
+      Channels: 1,
+      AudioQuality: 'Low',
+      AudioEncoding: 'ulaw',
+      AudioEncodingBitRate: 128000
+    });
+  }
 };
